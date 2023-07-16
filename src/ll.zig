77ee3b40195a5d37e7466fd6eb0c39bbf58e3456
Paul Colusso <paulcolusso@gmail.com>
2023-07-16 11:15:50 +1000

    working linked list

--- src/ll.zig
+++ src/ll.zig
@@ -3,41 +3,66 @@ const std = @import("std");
 pub fn LinkedList(comptime T: type) type {
     return struct {
         pub const Node = struct {
-            elem: T,
+            value: T,
             next: ?*Node = null,
 
-            pub fn get_last(self: *Node) ?*Node {
-                var current = self.next;
-                while (true) {
-                    const next = current.next;
-                    if (next == null) {
-                        return current;
-                    }
-                }
-            }
-
             pub fn print(self: *Node) void {
-                std.debug.print("Am {any} holding ({any}) next -> {any}", .{ &self, self.elem, self.next });
+                std.debug.print("Am {any} next -> {?}\n", .{ &self, &self.next });
             }
         };
 
-        head: ?*Node = null,
+        head: ?*Node,
+        allocator: *std.mem.Allocator,
+
+        pub fn init(allocator: *std.mem.Allocator) LinkedList(T) {
+            return LinkedList(T){ .allocator = allocator, .head = null };
+        }
+
+        pub fn deinit(self: *LinkedList(T)) void {
+            var iter = self.iterator();
+            while (iter.next()) |node| {
+                self.allocator.destroy(node);
+            }
+        }
 
         pub fn empty(self: *LinkedList(T)) bool {
             return self.head == null;
         }
 
-        pub fn prepend(self: *LinkedList(T), elem: T) void {
-            var node = Node{ .elem = elem, .next = self.head };
-            self.head = &node;
+        pub fn prepend(self: *LinkedList(T), elem: T) !void {
+            var node = try self.allocator.create(Node);
+            node.*.value = elem;
+
+            node.*.next = self.head;
+            self.head = node;
         }
 
         pub fn print(self: *LinkedList(T)) void {
-            var current = self.head;
-            while (current) |node_ptr| {
-                node_ptr.print();
-                current = if (node_ptr.next) |next_ptr| next_ptr else null;
+            var iter = self.iterator();
+            while (iter.next()) |node| {
+                std.debug.print("Node: {any}\n", .{node.value});
             }
+        }
+
+        pub const Iterator = struct {
+            list: *LinkedList(T),
+            cursor: ?*Node,
+
+            pub fn init(list: *LinkedList(T)) Iterator {
+                return Iterator{ .list = list, .cursor = list.head orelse null };
+            }
+
+            pub fn next(self: *Iterator) ?*Node {
+                var current = self.cursor;
+                if (current) |c| {
+                    self.cursor = c.next;
+                }
+                return current;
+            }
+        };
+
+        pub fn iterator(self: *LinkedList(T)) Iterator {
+            return Iterator.init(self);
         }
     };
 }
